416ee1929e4cb165513c2880123b7bfb634d9393
Paul Fox <p.fox@lorgames.com>
2014-11-10 13:35:50 +0000

    Removed strange dependency on Lua. Removed FMOD (will add a open source one soon, API will be the same)

--- Ascendgine/Audio.cpp
+++ Ascendgine/Audio.cpp
@@ -1,70 +1,42 @@
 #include "Audio.h"
 #include <limits.h>
-#include <FMOD\fmod.hpp>
 
 struct AudioObject
 {
-  FMOD::Channel* sound;
+  int noObj;
 };
-
-FMOD::System* system;
 
 void Audio_Init()
 {
-  FMOD::System_Create(&system);
-  
-  if (system)
-  {
-    system->init(32, FMOD_INIT_NORMAL, 0);
-  }
+
 }
 
 void Audio_Destroy()
 {
-  if (system)
-    system->release();
+
 }
 
 void Audio_Update(float dt)
 {
-  if (system)
-    system->update();
+
 }
 
 void Audio_Play(char* filename, AudioObject* pAudioObject, float volume /*= 0.5f*/, bool loop /*= false*/)
 {
-  pAudioObject = new AudioObject();
-  pAudioObject->sound = nullptr;
 
-  FMOD::Sound* sound;
-  system->createSound(filename, FMOD_SOFTWARE, 0, &sound);
-  sound->setLoopCount(9999 * (int)loop);
-  sound->setMode(loop ? FMOD_LOOP_NORMAL : FMOD_LOOP_OFF);
-
-  system->playSound(FMOD_CHANNEL_FREE, sound, false, &pAudioObject->sound);
-  pAudioObject->sound->setVolume(volume);
 }
 
 void Audio_Cleanup(AudioObject* pAudioObject)
 {
-  pAudioObject->sound->stop();
-  FMOD::Sound* sound;
-  pAudioObject->sound->getCurrentSound(&sound);
-  sound->release();
+
 }
 
 bool Audio_IsPlaying(AudioObject* pAudioObject)
 {
-  bool success = false;
-  if (pAudioObject == nullptr || pAudioObject->sound == nullptr)
-    return success;
-  pAudioObject->sound->isPlaying(&success);
-  return success;
+  return false;
 }
 
 void Audio_StopPlaying(AudioObject* pAudioObject)
 {
-  if (pAudioObject == nullptr || pAudioObject->sound == nullptr)
-    return;
-  pAudioObject->sound->stop();
+
 }
